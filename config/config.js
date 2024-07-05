c84488231092e5300fa537b069becf20d43b0684
Rijvan Juneja <rijvan.juneja@esparkbizmail.com>
2024-07-05 19:09:18 +0530

    Intial commit with folder structure

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const dotEnv = require("dotenv")
+dotEnv.config({ path: `.env` });
+module.exports = {
+  "development": {
+    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dialect: "mysql"
+  }
+};
+
